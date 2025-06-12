ddd9638a1e65f64c33aa5b56174a5eee5268452b
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-12 14:47:33 +0700

    Refactor LayoutUser component to include NavBar and improve structure

--- frontend/src/components/Layout/User/index.js
+++ frontend/src/components/Layout/User/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CONSTANTS from '@constants';
+import NavBar from '@components/NavBar';
 
 export default function LayoutUser({ children, className }) {
   const today = new Date();
@@ -8,8 +9,11 @@ export default function LayoutUser({ children, className }) {
 
   return (
     <div className={`mt-2 px-2 dark:text-white ${className}`}>
-      <div>{children}</div>
-      <div className="mt-5 text-center font-light text-[13px] text-pink-500 dark:text-gray-300">
+      <div>
+        <NavBar />
+        {children}
+      </div>
+      <div className="mt-5 text-center font-light text-[13px] text-gray-900 dark:text-gray-200">
         &#169; {currentYear} {CONSTANTS.SITE_NAME}
       </div>
     </div>
