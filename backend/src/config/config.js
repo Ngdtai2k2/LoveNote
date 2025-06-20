c433c9be422db0005c0aa9babf9aa06ccd26db13
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-20 20:31:12 +0700

    Fix database dialect configuration to use 'mysql' in config files

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'mysql',
     dialectModule: require('mysql2'),
   },
 };
