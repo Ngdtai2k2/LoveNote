d87ef9b4a86f24ed066133ea2efd47c925770f57
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-21 18:12:57 +0700

    Add production database configuration to config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -10,4 +10,14 @@ module.exports = {
     dialect: 'mysql',
     dialectModule: require('mysql2'),
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    logging: false,
+  },
 };
