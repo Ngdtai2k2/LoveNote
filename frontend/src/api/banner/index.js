1aa23363ddf29e784d5fc8f58a2f375947ce7163
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-21 20:46:34 +0700

    Add banner API service with getAll method

---
+++ frontend/src/api/banner/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+import API_ENDPOINTS from '@utils/api';
+
+export const bannerAPI = {
+  getAll: async isActive => {
+    const data = await axios.get(`${API_ENDPOINTS.BANNER.GET_ALL}?is_active=${isActive}`);
+    return data;
+  },
+};
