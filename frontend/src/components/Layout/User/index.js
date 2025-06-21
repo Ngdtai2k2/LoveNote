5ad6ae97e556a9182e88117b3e54bb73539934e9
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-21 22:59:30 +0700

    Refactor LayoutUser component

--- frontend/src/components/Layout/User/index.js
+++ frontend/src/components/Layout/User/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CONSTANTS from '@constants';
+
 import NavBar from '@components/NavBar';
+import Footer from '@components/Footer';
 
 export default function LayoutUser({ children, className }) {
-  const today = new Date();
-  const currentYear = today.getFullYear();
-
   return (
     <div className={`mt-2 px-2 dark:text-white ${className}`}>
       <div>
         <NavBar />
         {children}
       </div>
-      <div className="mt-5 text-center font-light text-[13px] text-gray-900 dark:text-gray-200">
-        &#169; {currentYear} {CONSTANTS.SITE_NAME}
-      </div>
+      <Footer />
     </div>
   );
 }
