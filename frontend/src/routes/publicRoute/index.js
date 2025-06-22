c08d62c37f7712e817543510ce77e0f618eea7dc
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-22 19:33:11 +0700

    Refactor publicRoute paths to use ROUTES constants for better maintainability

--- frontend/src/routes/publicRoute/index.js
+++ frontend/src/routes/publicRoute/index.js
@@ -1,20 +1,21 @@
 import SignIn from '@pages/User/Auth/SignIn';
 import SignUp from '@pages/User/Auth/SignUp';
 import Home from '@pages/Home';
+import ROUTES from '@constants/routes';
 
 const publicRoute = [
   {
-    path: '/sign-in',
+    path: ROUTES.AUTH.SIGN_IN,
     element: <SignIn />,
     hideWhenAuthenticated: true,
   },
   {
-    path: '/sign-up',
+    path: ROUTES.AUTH.SIGN_UP,
     element: <SignUp />,
     hideWhenAuthenticated: true,
   },
   {
-    path: '/',
+    path: ROUTES.HOME,
     element: <Home />,
     hideWhenAuthenticated: false,
   },
