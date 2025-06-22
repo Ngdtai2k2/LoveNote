0568a35e339fa7e4b575554ea5381e393a903228
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-22 14:21:30 +0700

    Set default value for LayoutComponent in renderRoutes function

--- frontend/src/helpers/renderRoutes/index.js
+++ frontend/src/helpers/renderRoutes/index.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import { Route } from 'react-router-dom';
+
 import ProtectedRoute from '@routes/protectedRoute/protectedRoute';
 
-export const renderRoutes = (routes, LayoutComponent) =>
+export const renderRoutes = (routes, LayoutComponent = React.Fragment) =>
   routes.map(({ path, element, requiredRole }, index) => (
     <Route
       key={index}
