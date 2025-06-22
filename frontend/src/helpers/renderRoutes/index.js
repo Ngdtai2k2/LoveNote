1d5a46af25888f797b29bf660e180b2f255f13b7
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-22 20:11:13 +0700

    Enhance renderRoutes function to support private routes and authentication checks

--- frontend/src/helpers/renderRoutes/index.js
+++ frontend/src/helpers/renderRoutes/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import ProtectedRoute from '@routes/protectedRoute/protectedRoute';
+import PrivateRoute from '@routes/privateRoute/privateRoute';
 
-export const renderRoutes = (routes, LayoutComponent = React.Fragment) =>
-  routes.map(({ path, element, requiredRole }, index) => (
-    <Route
-      key={index}
-      path={path}
-      element={
-        <ProtectedRoute
-          requiredRole={requiredRole}
-          element={<LayoutComponent>{element}</LayoutComponent>}
-        />
-      }
-    />
-  ));
+export const renderRoutes = (routes, LayoutComponent = React.Fragment, isAuthenticated = false) =>
+  routes.map(({ path, element, isPrivate, requiredRole }, index) => {
+    let content = <LayoutComponent>{element}</LayoutComponent>;
+
+    if (requiredRole) {
+      content = <ProtectedRoute requiredRole={requiredRole} element={content} />;
+    } else if (isPrivate) {
+      content = <PrivateRoute isAuth={isAuthenticated} element={content} />;
+    }
+
+    return <Route key={index} path={path} element={content} />;
+  });
