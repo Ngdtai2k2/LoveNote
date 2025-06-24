755441c8e4f7af92d629806d470886f997708c73
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-24 14:11:03 +0700

    Add updateProfile function to userAPI

--- frontend/src/api/user/index.jsx
+++ frontend/src/api/user/index.jsx
@@ -1,6 +1,7 @@
 import API_ENDPOINTS from '@utils/api';
 import { removeUser, setUser } from '@redux/slice/user';
 import { signOutSuccess } from '@redux/slice/auth';
+import { toast } from 'react-fox-toast';
 
 export const userAPI = {
   getCurrentUser: async (dispatch, axiosJWT) => {
@@ -10,6 +11,23 @@ export const userAPI = {
     } catch {
       dispatch(removeUser());
       dispatch(signOutSuccess());
+    }
+  },
+  updateProfile: async (userId, data, axiosJWT) => {
+    try {
+      const res = await axiosJWT.put(API_ENDPOINTS.USER.UPDATE(userId), data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      toast.success(res.data.message, {
+        position: 'top-right',
+      });
+      return res;
+    } catch (error) {
+      toast.error(error.response?.data.message, {
+        position: 'top-right',
+      });
     }
   },
 };
