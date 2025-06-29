2de3371cd59df723e2e49ec905b34982013e71ac
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-06-30 01:47:42 +0700

    Add GalaxyLoveLetter template to TEMPLATE_ROUTE

--- frontend/src/constants/routes/template.jsx
+++ frontend/src/constants/routes/template.jsx
@@ -1,7 +1,9 @@
 import MatrixLoveRain from '@templates/matrixLoveRain';
+import GalaxyLoveLetter from '@templates/galaxyLoveLetter';
 
 const TEMPLATE_ROUTE = {
   'matrix-love-rain': MatrixLoveRain,
+  'galaxy-love-letter': GalaxyLoveLetter,
 };
 
 export default TEMPLATE_ROUTE;
