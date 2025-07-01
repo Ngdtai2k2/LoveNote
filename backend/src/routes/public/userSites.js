a22da251ae729b4b7aa3463fe286410bf53031a0
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-07-01 16:53:14 +0700

    Refactor upload handling in userSites route to simplify middleware usage

--- backend/src/routes/public/userSites.js
+++ backend/src/routes/public/userSites.js
@@ -1,20 +1,17 @@
 const router = require('express').Router();
 const userSitesController = require('@controllers/public/userSites');
 const verifyMiddleware = require('@middlewares/verifyToken');
-const uploadMiddleware = require('@middlewares/upload');
+const upload = require('@middlewares/upload')();
 
 router.get('/configs', userSitesController.getConfigSite);
 
 router.post(
   '/configs',
   verifyMiddleware.token,
-  (req, res, next) => {
-    const upload = uploadMiddleware().fields([
-      { name: 'file', maxCount: 1 },
-      { name: 'images', maxCount: 5 },
-    ]);
-    upload(req, res, next);
-  },
+  upload.fields([
+    { name: 'audio', maxCount: 1 },
+    { name: 'images', maxCount: 5 },
+  ]),
   userSitesController.createConfigSite
 );
 
