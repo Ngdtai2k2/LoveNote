70b4f27ce85338d3169a3b92412deb744be19bf9
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-07-02 00:01:16 +0700

    Add MatrrixRainWithParticles to TEMPLATE_ROUTE

--- frontend/src/constants/routes/template.jsx
+++ frontend/src/constants/routes/template.jsx
@@ -1,11 +1,13 @@
 import MatrixLoveRain from '@templates/matrixLoveRain';
 import GalaxyLoveLetter from '@templates/galaxyLoveLetter';
 import HeartBeatVisualizer from '@templates/heartBeatVisualizer';
+import MatrrixRainWithParticles from '@templates/matrixRainWithParticles';
 
 const TEMPLATE_ROUTE = {
   'matrix-love-rain': MatrixLoveRain,
   'galaxy-love-letter': GalaxyLoveLetter,
-  'heart-beat-visualizer': HeartBeatVisualizer
+  'heart-beat-visualizer': HeartBeatVisualizer,
+  'matrix-rain-text': MatrrixRainWithParticles,
 };
 
 export default TEMPLATE_ROUTE;
