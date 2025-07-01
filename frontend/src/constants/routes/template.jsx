d0dbdb851821839260efa14e1465c40d2d9fff52
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-07-01 17:40:15 +0700

    Add HeartBeatVisualizer to TEMPLATE_ROUTE for additional template support

--- frontend/src/constants/routes/template.jsx
+++ frontend/src/constants/routes/template.jsx
@@ -1,9 +1,11 @@
 import MatrixLoveRain from '@templates/matrixLoveRain';
 import GalaxyLoveLetter from '@templates/galaxyLoveLetter';
+import HeartBeatVisualizer from '@templates/heartBeatVisualizer';
 
 const TEMPLATE_ROUTE = {
   'matrix-love-rain': MatrixLoveRain,
   'galaxy-love-letter': GalaxyLoveLetter,
+  'heart-beat-visualizer': HeartBeatVisualizer
 };
 
 export default TEMPLATE_ROUTE;
