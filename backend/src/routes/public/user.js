3e7c1b63d7503c1ca86881503d64caec13981031
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-07-01 16:41:19 +0700

    Add upload middleware to user profile update route

--- backend/src/routes/public/user.js
+++ backend/src/routes/public/user.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const userController = require('@controllers/public/user');
 const verifyMiddleware = require('@middlewares/verifyToken');
+const uploadMiddleware = require('@middlewares/upload');
+
+const upload = uploadMiddleware().fields([{ name: 'avatar', maxCount: 1 }]);
 
 router.put(
   '/:userId',
   verifyMiddleware.ownership,
+  upload,
   userController.updateProfile
 );
 
