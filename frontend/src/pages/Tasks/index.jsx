7a96c568893bb3ca410432cdb589f2d613dc39ee
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-07-15 09:54:36 +0700

    Refactor code

--- frontend/src/pages/Tasks/index.jsx
+++ frontend/src/pages/Tasks/index.jsx
@@ -37,8 +37,6 @@ export default function Tasks() {
       if (res?.data?.url) {
         window.location.href = res.data.url;
       }
-    } catch (error) {
-      console.error(error);
     } finally {
       setLoadingProviderId(null);
     }
