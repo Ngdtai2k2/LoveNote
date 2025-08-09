39791543dae955fa4294eee5d45a65a7d79b537c
ngdtai2k2 <ngdtai2k2@gmail.com>
2025-08-09 20:30:37 +0700

    Refactor UI

--- frontend/src/pages/Admin/LogViewer/index.jsx
+++ frontend/src/pages/Admin/LogViewer/index.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { DocumentTextIcon } from '@heroicons/react/24/solid';
+
+import { useDocumentTitle } from '@hooks/useDocumentTitle';
 import { useAxios } from '@hooks/useAxiosJWT';
 import { logsApi } from '@api/admin/logs';
 
@@ -10,6 +13,8 @@ export default function LogViewer() {
 
   const { t, i18n } = useTranslation('admin');
   const { axiosJWT } = useAxios(i18n.language);
+
+  useDocumentTitle(t('logs.title'));
 
   const preRef = useRef(null);
 
@@ -36,8 +41,8 @@ export default function LogViewer() {
 
   return (
     <div className="p-4 md:mx-5">
-      <h1 className="text-base mb-4 font-semibold text-gray-900 dark:text-gray-100">
-        {t('logs.auto_delete_sites')}
+      <h1 className="flex gap-1 item-center text-xl mb-4 font-semibold text-gray-900 dark:text-gray-100">
+        <DocumentTextIcon className='h-6 w-6'/> {t('logs.title')}
       </h1>
 
       {loading && (
@@ -48,15 +53,20 @@ export default function LogViewer() {
       )}
 
       {!loading && (
-        <pre
-          ref={preRef}
-          className="p-4 font-mono rounded overflow-auto max-h-[70vh] whitespace-pre-wrap break-words bg-gray-100 
+        <>
+          <h2 className="text-sm mb-2 font-medium italic text-gray-700 dark:text-gray-300">
+            {t('logs.auto_delete_sites')}
+          </h2>
+          <pre
+            ref={preRef}
+            className="p-4 font-mono rounded overflow-auto max-h-[70vh] whitespace-pre-wrap break-words bg-gray-100 
           text-gray-900 dark:bg-black dark:text-white border border-gray-300 dark:border-gray-700 scrollbar-thin 
            scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-thumb-gray-600 dark:scrollbar-track-gray-900"
-          style={{ whiteSpace: 'pre-wrap' }}
-        >
-          {logData || t('logs.no_logs')}
-        </pre>
+            style={{ whiteSpace: 'pre-wrap' }}
+          >
+            {logData || t('logs.no_logs')}
+          </pre>
+        </>
       )}
     </div>
   );
